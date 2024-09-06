fbf741b717faa5a001ddc7644218448ced60005b
Lucas-g39 <lgfc4@discente.ifpe.edu.br>
2024-09-05 21:25:52 -0300

    RPG JOGO

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import "../styles/MenuP.css"
+
+  </script>
+
+<br />
+<a class="menu" id="botaoplay" href="/Introdução" src="/images/detalhe1">PRESSIONE PARA JOGAR</a>
+<br />
+<a class="menu" id="botaoquit" href="/quit">QUIT</a> 
---
+++ src/routes/Introdução/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import "../../styles/introducao.css"
+</script>
+
+<div style="overflow:scroll; width: 1000px; height:850px">
+    <h1>WARNING!!</h1>
+    <h2>SUAS ESCOLHAS TEM CONSEQUÊNCIAS!</h2>
+    <img class="jogar" src="/images/imagem.png" alt="foto"/>
+    <h2>A narrativa vai se dar conforme suas escolhas. <br> Leia com calma e escolha o que mais te agradar.</h2>
+    <h1 class="sobre" id="nomesobre">Sobre</h1>
+    <img class="sobre" src="/images/logoIFPE.png" alt="logo IPFE"/>
+    <p>
+      Autores:<br> Filipe J. <br>Lionardo M. <br> Lucas G. <br> Pedro L. <br> Reynaldo S 
+    </p>
+</div>
+
+<a class="introducao" id="botaovoltarmenu" href="/">Voltar</a>
+<a class="introducao" id="botaogameplay" href="/gameplay" >Pular</a>
